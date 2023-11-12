cf159dcde734f0085cbe8f6ec21e2539f5c33ca1
crnicavic <cavic5000@tutanota.com>
2023-11-12 22:00:44 +0100

    elitis is now a float

--- genetic.h
+++ genetic.h
@@ -15,13 +15,22 @@
                         ASSERT(v != NULL, FAIL, MALLOC_FAIL(v))
 
 /* v- variable(name) r-rows c-cols t-type */
-#define ALLOC_MATRIX(v, r, c, t)  v = (t**) malloc(sizeof(t*) * r); \
+#define ALLOC_MATRIX(v, r, c, t) do { int rr = 0;    \
+                            v = (t**) malloc(sizeof(t*) * r); \
                             ASSERT(v != NULL, FAIL, MALLOC_FAIL(v)); \
-                            rr=0; \
                             for(;rr < r; ++rr) { \
-                                v[rr] = (t*) malloc(sizeof(t) * c); \
-                                ASSERT(v[rr] != NULL, FAIL, MALLOC_FAIL(v)); \
-                            }
+                            v[rr] = (t*) malloc(sizeof(t) * c); \
+                            ASSERT(v[rr] != NULL, FAIL, MALLOC_FAIL(v)); } \
+                            } while(0);
+
+/* v-variable r-rows s-row to start from */
+#define FREE_MATRIX(v, r, s) do { int rr = s; \
+                                for(; rr < r; rr++) { \
+                                    free(v[rr]); \
+                                } \
+                                if(!s) { free(v); } } while(0);
+
+#define swap(x, y, T) do {T swap = x; x = y; y = swap;} while(0);
 
 #define _POSIX_C_SOURCE 200809L
 #define TRUE 1
@@ -41,27 +50,28 @@ typedef struct gaconf{
     int size;       /* population size */
     int tour_size;  /* the size of the n/2 tournament */
     float mut_rate; /* probability of mutation from 0 to 1 */
-    int elitis;     /* count of units to conserve to elitis */
+    float elitis;   /* percentage of units to be taken into next gen*/ 
     int gens;       /* generation count */
     int find_max;   /* 0 if looking for maximum */
     int sel_alg;    /* selection algorithm */
 }gaconf;
 
-static int create_matrix(int rows, int cols);
-static int create_array(int size);
 static void generate_initial_pop(float *ranges);
 static void calculate_costs(float(*f)(float*));
 static int greater(float a, float b);
 static int lesser(float a, float b);
-int quicksort_partition(int start, int stop);
-void quicksort_pop(int start, int stop);
-static void tournament(int participant_count);
-static float prob_max(float cost);
-static float prob_min(float cost);
+static int partition(int start, int stop);
+static int find_kth(int start, int stop, int k);
+static int* max_min(float *arr);
+static void prob_max(int* max_min);
+static void prob_min(int* max_min);
+static inline int spin();
 static void roulette(int find_max);
+static int tournament(int participant_count); 
+static void brackets(int participant_count);
 static void crossover_sym();
 static void mutation(float mut_rate);
 static void elitism(int elitis);
 static int ga_init(gaconf *ga);
+static void free_ga(gaconf *ga);
 float* ga(gaconf* ga, float (*func)(float*));
-
