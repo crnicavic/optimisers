0cd1c27dc9292180028bcbfe00afd8e9c208409d
crnicavic <cavic5000@tutanota.com>
2025-02-07 14:30:45 +0100

    add nix shell file

---
+++ ga/shell.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+mkShell {
+	NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+        stdenv.cc.cc
+  		      zlib
+  			    ];
+	NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+
+	buildInputs = [
+		pkg-config
+		python311
+		python311Packages.pip
+		python311Packages.tkinter						
+		gcc
+		gdb
+		cmake
+		gnumake
+		];
+		shellHook = ''
+				export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+				source .venv/bin/activate
+		'';
+}
