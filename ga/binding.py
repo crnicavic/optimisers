c2f2e44aaab22db05becd021ae64f5220b7ae803
crnicavic <cavic5000@tutanota.com>
2025-02-07 14:29:52 +0100

    create basic bindings, no idea if they work

---
+++ ga/binding.py
@@ -0,0 +1,24 @@
+# making a neural network trained with the genetic algorithm
+
+import ctypes
+
+
+class ga_conf(ctypes.Structure):
+	_fields_ = [("ranges"), ctypes.pointer(ctypes.c_float),
+				("dims"), ctypes.c_int,
+				("size"), ctypes.c_int,
+				("tour_size"), ctypes.c_int,
+				("mut_rate"), ctypes.c_float,
+				("elitis"), ctypes.c_float,
+				("gens"), ctypes.c_int,
+				("find_max"), ctypes.c_int,
+				("sel_alg"), ctypes.c_int]
+
+
+# target function type
+target_type = ctypes.CFUNCTYPE
+
+fun = ctypes.CDLL("libgenetic.so")
+
+fun.myFunction.argtypes = []
+
